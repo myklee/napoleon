bd327af42dc0407e6cc3b606ecf88691bb18bf68
myklee <myklee@gmail.com>
2011-08-13 16:55:08 -0400

    basic pig latin translation funcionality done

--- js/custom.js
+++ js/custom.js
@@ -5,12 +5,18 @@ $(document).ready(function() {
   	var english = $('#english').val();
   	var split = english.split(" ");
   	var $translated = $('#translated');
-  	$.each(split, function() {
-  		var translated = ''
-  		var word = this.toString() + '-ay';	
-  		
-  		alert(word);
+  	var all = '';
+  	
+  	$.each(split, function() { 
+  		var word = this.toString() + '-' + this.substring(0,1) + 'ay';
+  		if (word.length > 5) {
+  		word = word.substring(1);
+  		} else {};
+//  		var translated = $pig.join(" ");
+		all = all + word + ' ';
+		return all;
   	  	});
- 
-  });
+  	  	
+  	  	$translated.text(all);
+	  }); 
 });
